4fd1eeb49921b0f58f3ae8a35111568cab352a3e
Marc-Antoine Berger <marberge@z3r9p1.42lyon.fr>
2025-11-04 19:21:43 +0100

    finished ex06

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marberge <marberge@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 18:49:21 by marberge          #+#    #+#             */
+/*   Updated: 2025/11/04 19:13:52 by marberge         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_alphabet(void)
+{
+	char	a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		ft_putchar(a);
+		a++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
