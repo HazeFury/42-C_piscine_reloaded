2fb5f2ce54d892a22ecd3272dcd607a4b7d5df17
Marc-Antoine Berger <marberge@z3r9p1.42lyon.fr>
2025-11-04 19:43:33 +0100

    finished ex07

--- ex07/ft_print_numbers.c
+++ ex07/ft_print_numbers.c
@@ -6,7 +6,7 @@
 /*   By: marberge <marberge@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/04 19:35:09 by marberge          #+#    #+#             */
-/*   Updated: 2025/11/04 19:36:01 by marberge         ###   ########lyon.fr   */
+/*   Updated: 2025/11/04 19:43:06 by marberge         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,19 @@ void	ft_putchar(char c);
 
 void	ft_print_numbers(void)
 {
+	char	i;
 
+	i = 48;
+	while (i <= 57)
+	{
+		ft_putchar(i);
+		i++;
+	}
 }
 
-
+/*
+int	main(void)
+{
+	ft_print_numbers();
+}
+*/
